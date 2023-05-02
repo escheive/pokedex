60f1a568643f4ff0e90ec1f333ab6c4f188cc5e6
escheive <scheiveerik@gmail.com>
2023-05-02 15:49:23 -0700

    now pokemon types display background colors dynamically based on the type

--- src/screens/DetailsScreen.tsx
+++ src/screens/DetailsScreen.tsx
@@ -1,11 +1,136 @@
-import React from 'react';
-import { View, Text, StyleSheet, Button } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, StyleSheet, Button, Image } from 'react-native';
 
-const HomeScreen = ({ navigation }) => {
+type TypeProps = {
+    type: {
+        name: string;
+    };
+};
+
+type PokemonProps = {
+  name: string;
+  id: number;
+  types: TypeProps[];
+}
+
+type DetailsScreenProps = {
+  route: {
+    params: {
+      pokemon: PokemonProps;
+    };
+  };
+  navigation: any;
+}
+
+
+const DetailsScreen = ({ route, navigation }: DetailsScreenProps) => {
+  const { pokemon } = route.params;
+
+  // Define a function to return styles for each type
+    const getTypeStyle = (typeName: string) => {
+      switch (typeName) {
+        case 'normal':
+          return {
+            backgroundColor: 'gray',
+          };
+        case 'fire':
+          return {
+            backgroundColor: 'orange',
+          };
+        case 'water':
+          return {
+            backgroundColor: 'blue',
+          };
+        case 'grass':
+          return {
+            backgroundColor: '#78c850',
+          };
+        case 'electric':
+          return {
+            backgroundColor: 'yellow',
+          };
+        case 'ice':
+          return {
+            backgroundColor: 'cyan',
+          };
+        case 'fighting':
+          return {
+            backgroundColor: 'red',
+          };
+        case 'poison':
+          return {
+            backgroundColor: 'purple',
+          };
+        case 'ground':
+          return {
+            backgroundColor: 'brown',
+          };
+        case 'flying':
+          return {
+            backgroundColor: 'sky-blue',
+          };
+        case 'psychic':
+          return {
+            backgroundColor: 'pink',
+          };
+        case 'bug':
+          return {
+            backgroundColor: 'lime',
+          };
+        case 'rock':
+          return {
+            backgroundColor: 'sienna',
+          };
+        case 'ghost':
+          return {
+            backgroundColor: 'violet',
+          };
+        case 'dragon':
+          return {
+            backgroundColor: 'indigo',
+          };
+        case 'dark':
+          return {
+            backgroundColor: 'black',
+          };
+        case 'steel':
+          return {
+            backgroundColor: 'silver',
+          };
+        case 'fairy':
+          return {
+            backgroundColor: 'magenta',
+          };
+        default:
+          return {};
+      }
+    };
+
+    const getTypeBackgroundStyle = (types: TypeProps[]) => {
+      const stylesArray = types.map((type) => getTypeStyle(type.type.name));
+      return stylesArray.filter(style => Object.keys(style).length > 0);
+    };
+
+
+
+
   return (
     <View style={styles.container}>
-      <Text style={styles.heading}>Welcome to My App!</Text>
-      <Text style={styles.body}>This is the details screen.</Text>
+      <Text style={styles.heading}>{pokemon.name}</Text>
+      <Image
+        style={styles.image}
+        source={{ uri: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.id}.png` }}
+      />
+      <Text style={styles.detailsText}>Type:</Text>
+      <View style={styles.typesContainer}>
+        {pokemon.types.map((type) => (
+          <View key={type.type.name} style={[styles.type, ...getTypeBackgroundStyle([type])]}>
+            <Text style={styles.typeText}>
+              {type.type.name}
+            </Text>
+          </View>
+        ))}
+      </View>
     </View>
   );
 };
@@ -21,10 +146,27 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     marginBottom: 16,
   },
-  body: {
+  detailsText: {
     fontSize: 16,
-    textAlign: 'center',
+    fontWeight: 'bold',
+    marginTop: 16,
+  },
+  image: {
+    width: 200,
+    height: 200,
+    marginBottom: 16,
+  },
+  typesContainer: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    marginBottom: 16,
+  },
+  type: {
+    color: 'white',
+    padding: 8,
+    borderRadius: 8,
+    marginHorizontal: 8,
   },
 });
 
-export default HomeScreen;
+export default DetailsScreen;
--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -13,17 +13,17 @@ const HomeScreen = ({ navigation, pokemonList }: Props) => {
         navigation.navigate('Details', { pokemon });
     };
 
-    const renderItem = ({ item }: { item: Pokemon }) => {
+    const renderItem = ({ item: pokemon }: { item: Pokemon }) => {
         const screenWidth = Dimensions.get('window').width;
         const itemWidth = screenWidth / 2 - 15;
         return (
             <View style={[styles.itemContainer, { width: itemWidth}]}>
-                <TouchableOpacity style={styles.itemCard} onPress={() => handlePress(item)}>
-                    <Text>{item.id}</Text>
-                    <Text style={styles.itemName}>{item.name.charAt(0).toUpperCase() + item.name.slice(1)}</Text>
+                <TouchableOpacity style={styles.itemCard} onPress={() => handlePress(pokemon)}>
+                    <Text>{pokemon.id}</Text>
+                    <Text style={styles.pokemonName}>{pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1)}</Text>
                     <Image
                         style={styles.image}
-                        source={{ uri: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${item.id}.png` }}
+                        source={{ uri: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.id}.png` }}
                     />
                 </TouchableOpacity>
             </View>
