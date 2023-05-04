e99702cf668e0dde0983c76f2f7b632898ffe082
escheive <scheiveerik@gmail.com>
2023-05-04 15:28:14 -0700

    updated the profile stack and screen so that there is no more warning about nested names

--- src/screens/ProfileScreen.tsx
+++ src/screens/ProfileScreen.tsx
@@ -49,8 +49,6 @@ const ProfileScreen = () => {
     setIsEditing(false);
   };
 
-  console.log(favorites)
-
   return (
     <View style={styles.container}>
       <Text style={styles.heading}>Welcome to your profile!</Text>
