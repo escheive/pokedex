edb62b3fb67d9644e4667895fb1c8c92199458ff
escheive <scheiveerik@gmail.com>
2023-05-05 14:26:28 -0700

    added additional comments to the details screen code

--- src/screens/DetailsScreen.tsx
+++ src/screens/DetailsScreen.tsx
@@ -33,76 +33,91 @@ type DetailsScreenProps = {
 
 
 const DetailsScreen = ({ route, navigation }: DetailsScreenProps) => {
-  const { pokemon } = route.params;
-  const [isFavorite, setIsFavorite] = useState(false);
-  const [pokemonAbilities, setPokemonAbilities] = useState([]);
+    // Grab our pokemon data that was pulled in our app.tsx from params
+    const { pokemon } = route.params;
+    // useState to update if a pokemon was favorited or unfavorited without refreshing
+    const [isFavorite, setIsFavorite] = useState(false);
+    // useState to track pokemonAbilities for each individual pokemon when their page is pulled up
+    const [pokemonAbilities, setPokemonAbilities] = useState([]);
+    // Declare an array so that we can assign colors for this pokemon based on its type
+    let pokemonColors = [];
 
-  let pokemonColors = [];
+    // Function to fetch the ability information as abilities are stored in a separate part of the PokeApi
+    const fetchAbility = async (ability: AbilityProps) => {
+        // Fetch the url provided for the pokemons ability in the pokemon info
+        const abilityDefinitionResponse = await fetch(ability.ability.url);
+        // assign the returned json to a variable so that it can be handled
+        const abilityDefinition = await abilityDefinitionResponse.json();
 
-//   if (pokemon.abilities) {
-//     pokemon.abilities.map(async (ability, index) => {
-//         let abilityDefinitionResponse = await fetch(ability.ability.url)
-//         let abilityDefinition = await abilityDefinitionResponse.json();
-//         pokemonAbilities.push({ name: ability.ability.name, definition: abilityDefinition.effect_entries[1].effect })
-//         console.log(pokemonAbilities)
-//     });
-//   }
+        // Because not all abilities are formatted the same, this function will only return the english definition.
+        const getEnglishAbilityDescription = () => {
+            // Loop through all of the keys in effect_entries
+            for (const entry of abilityDefinition.effect_entries) {
+                // If the entry language is english, return data
+                if (entry.language.name == "en") {
+                    return entry.short_effect;
+                }
+            };
+            // If not english definition exists, return undefined
+            return undefined;
+        };
 
-  const fetchAbility = async (ability: AbilityProps) => {
-      const abilityDefinitionResponse = await fetch(ability.ability.url);
-      const abilityDefinition = await abilityDefinitionResponse.json();
-
-      const getEnglishAbilityDescription = () => {
-          for (const entry of abilityDefinition.effect_entries) {
-              if (entry.language.name == "en") {
-                  return entry.short_effect;
-              }
-          };
-          return undefined;
-      };
-
-      const effect_entries = getEnglishAbilityDescription();
-      const abilityData = { name: ability.ability.name, definition: effect_entries }
-      setPokemonAbilities((prevAbilities) => [...prevAbilities, abilityData]);
+        // Assign the return of the above function as a variable
+        const effect_entries = getEnglishAbilityDescription();
+        // assign the ability's name which is part of our original pokemon info, and the newly returned description as an object
+        const abilityData = { name: ability.ability.name, definition: effect_entries }
+        // Updated our abilities state by combining any previous info stored with our new info
+        setPokemonAbilities((prevAbilities) => [...prevAbilities, abilityData]);
     };
 
-  useEffect(() => {
-      checkIfFavorite();
-      const fetchAbilityData = async () => {
-        const promises = pokemon.abilities.map((ability) => fetchAbility(ability));
-        const result = await Promise.all(promises);
-      };
-      fetchAbilityData();
+    // useEffect to check if a pokemon is favorited and fetch ability info on component mount
+    useEffect(() => {
+        checkIfFavorite();
+        // Function to map through the pokemon abilities and then run fetchAbility function to grab the definitions for each
+        const fetchAbilityData = async () => {
+            const promises = pokemon.abilities.map((ability) => fetchAbility(ability));
+            const result = await Promise.all(promises);
+        };
+        fetchAbilityData();
     }, []);
 
+    // Function to check if a pokemon is favorited and update the page accordingly
     const checkIfFavorite = async () => {
+        // Run imported function from favorites file in utils folder to grab all favorited pokemon for a user and store them in a variable
         const favorites = await getFavorites();
+        // If this pokemon is one of the favorites, set as true
         if (favorites.some(pokemonObject => pokemonObject.id === pokemon.id)) {
             setIsFavorite(true);
         }
     };
 
-  const handleFavoritePress = async () => {
-      if (isFavorite) {
-        await removeFavoritePokemon(pokemon);
-        setIsFavorite(false);
-      } else {
-        const added = await addFavoritePokemon(pokemon);
-        setIsFavorite(added);
-      }
-  };
+    // Handle someone pressing the favorite pokemon button
+    const handleFavoritePress = async () => {
+        // If pokemon is already favorited, run imported function from favorites util to remove it from storage, then update page without needing refresh
+        if (isFavorite) {
+            await removeFavoritePokemon(pokemon);
+            setIsFavorite(false);
+        // If pokemon is not favorited, run imported function from utils folder to save it as a favorite, then update state to reflect immediately
+        } else {
+            const added = await addFavoritePokemon(pokemon);
+            setIsFavorite(added);
+        }
+    };
 
 
+    // Function to take the pokemons types, run function to get the corresponding color and then create an array of colors based on types
     const getTypeBackgroundStyle = (types: TypeProps[]) => {
       const stylesArray = types.map((type) => getTypeStyle(type.type.name));
       pokemonColors.push(stylesArray[0].backgroundColor)
       return stylesArray.filter(style => Object.keys(style).length > 0);
     };
 
+    // Variables to track the pokemons colors
     const stylesArray = getTypeBackgroundStyle(pokemon.types);
     pokemonColors = stylesArray.map((style) => style.backgroundColor);
     const gradientColors = pokemonColors.length < 2 ? [pokemonColors[0], '#FFFFFF'] : pokemonColors;
 
+    // Stylesheet for this screen
     const styles = StyleSheet.create({
       container: {
         height: '100%',
