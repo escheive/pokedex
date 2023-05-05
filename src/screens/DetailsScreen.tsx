c6a8b45f6e56a7a916bc446c0496c41477c740fe
escheive <scheiveerik@gmail.com>
2023-05-05 14:36:39 -0700

    fixed a bug where duplicate abilities would be added on page refresh

--- src/screens/DetailsScreen.tsx
+++ src/screens/DetailsScreen.tsx
@@ -1,14 +1,14 @@
+// Dependencies
 import React, { useState, useEffect } from 'react';
 import { ScrollView, View, Text, StyleSheet, Button, Image, FlatList } from 'react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import GradientBackground from '../components/GradientBackground';
 import LinearGradient from 'react-native-linear-gradient';
-import PokemonStats from '../components/PokemonStats';
-// Import the heart icon from the react-icons library
 import { FaHeart } from 'react-icons/fa';
+// Components
+import PokemonStats from '../components/PokemonStats';
+// Utils
 import { getFavorites, addFavoritePokemon, removeFavoritePokemon } from '../utils/favorites.tsx';
 import { getTypeStyle } from '../utils/typeStyle';
-import { PokemonAbilities } from '../components/PokemonAbilities';
 
 type TypeProps = {
     type: {
@@ -17,18 +17,18 @@ type TypeProps = {
 };
 
 type PokemonProps = {
-  name: string;
-  id: number;
-  types: TypeProps[];
+    name: string;
+    id: number;
+    types: TypeProps[];
 }
 
 type DetailsScreenProps = {
-  route: {
-    params: {
-      pokemon: PokemonProps;
+    route: {
+        params: {
+            pokemon: PokemonProps;
+        };
     };
-  };
-  navigation: any;
+    navigation: any;
 }
 
 
@@ -75,8 +75,10 @@ const DetailsScreen = ({ route, navigation }: DetailsScreenProps) => {
         checkIfFavorite();
         // Function to map through the pokemon abilities and then run fetchAbility function to grab the definitions for each
         const fetchAbilityData = async () => {
-            const promises = pokemon.abilities.map((ability) => fetchAbility(ability));
-            const result = await Promise.all(promises);
+            if (pokemonAbilities.length < pokemon.abilities.length) {
+                const promises = pokemon.abilities.map((ability) => fetchAbility(ability));
+                const result = await Promise.all(promises);
+            }
         };
         fetchAbilityData();
     }, []);
@@ -107,9 +109,9 @@ const DetailsScreen = ({ route, navigation }: DetailsScreenProps) => {
 
     // Function to take the pokemons types, run function to get the corresponding color and then create an array of colors based on types
     const getTypeBackgroundStyle = (types: TypeProps[]) => {
-      const stylesArray = types.map((type) => getTypeStyle(type.type.name));
-      pokemonColors.push(stylesArray[0].backgroundColor)
-      return stylesArray.filter(style => Object.keys(style).length > 0);
+        const stylesArray = types.map((type) => getTypeStyle(type.type.name));
+        pokemonColors.push(stylesArray[0].backgroundColor)
+        return stylesArray.filter(style => Object.keys(style).length > 0);
     };
 
     // Variables to track the pokemons colors
@@ -119,136 +121,141 @@ const DetailsScreen = ({ route, navigation }: DetailsScreenProps) => {
 
     // Stylesheet for this screen
     const styles = StyleSheet.create({
-      container: {
-        height: '100%',
-      },
-      card: {
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'flex-start',
-        borderWidth: 10,
-        borderColor: '#e3cc0c',
-        borderRadius: 10,
-        overflow: 'hidden',
-        margin: 10,
-        marginBottom: 20,
-        padding: 10,
-        backgroundColor: pokemonColors[0],
-        shadowColor: 'black',
-        shadowOffset: { width: 0, height: 2 },
-        shadowOpacity: 0.23,
-        shadowRadius: 2.62,
-        elevation: 4,
-        fontFamily: 'Arial, sans-serif',
-      },
-      heading: {
-        fontSize: 24,
-        fontWeight: 'bold',
-        alignSelf: 'flex-start'
-      },
-      header: {
-        flexDirection: 'row',
-        alignItems: 'center',
-      },
-      nameContainer: {
-        flexDirection: 'row',
-        alignItems: 'center',
-        justifyContent: 'space-between',
-        flex: 1,
-        marginHorizontal: 16,
-      },
-      hp: {
-        fontSize: 22,
-        fontWeight: '600',
-      },
-      detailsText: {
-        fontSize: 20,
-        fontWeight: 'bold',
-        marginTop: 0,
-        marginBottom: 4,
-      },
-      image: {
-        width: 300,
-        height: 300,
-        resizeMode: 'contain',
-      },
-      imageContainer: {
-        shadowColor: 'black',
-        shadowOffset: { width: 0, height: 1 },
-        shadowOpacity: 0.3,
-        shadowRadius: 2,
-        elevation: 5,
-        borderWidth: 10,
-        borderColor: '#dcb922',
-        marginBottom: 16,
-      },
-      typesContainer: {
-        flexDirection: 'row',
-        justifyContent: 'center',
-        marginBottom: 8,
-      },
-      type: {
-        paddingVertical: 10,
-        paddingHorizontal: 16,
-        borderRadius: 16,
-        marginHorizontal: 8,
-        borderWidth: 2,
-        borderColor: '#ffffff',
-      },
+        container: {
+            height: '100%',
+        },
+        card: {
+            flex: 1,
+            alignItems: 'center',
+            justifyContent: 'flex-start',
+            borderWidth: 10,
+            borderColor: '#e3cc0c',
+            borderRadius: 10,
+            overflow: 'hidden',
+            margin: 10,
+            marginBottom: 20,
+            padding: 10,
+            backgroundColor: pokemonColors[0],
+            shadowColor: 'black',
+            shadowOffset: { width: 0, height: 2 },
+            shadowOpacity: 0.23,
+            shadowRadius: 2.62,
+            elevation: 4,
+            fontFamily: 'Arial, sans-serif',
+        },
+        heading: {
+            fontSize: 24,
+            fontWeight: 'bold',
+            alignSelf: 'flex-start'
+        },
+        header: {
+            flexDirection: 'row',
+            alignItems: 'center',
+        },
+        nameContainer: {
+            flexDirection: 'row',
+            alignItems: 'center',
+            justifyContent: 'space-between',
+            flex: 1,
+            marginHorizontal: 16,
+        },
+        hp: {
+            fontSize: 22,
+            fontWeight: '600',
+        },
+        detailsText: {
+            fontSize: 20,
+            fontWeight: 'bold',
+            marginTop: 0,
+            marginBottom: 4,
+        },
+        image: {
+            width: 300,
+            height: 300,
+            resizeMode: 'contain',
+        },
+        imageContainer: {
+            shadowColor: 'black',
+            shadowOffset: { width: 0, height: 1 },
+            shadowOpacity: 0.3,
+            shadowRadius: 2,
+            elevation: 5,
+            borderWidth: 10,
+            borderColor: '#dcb922',
+            marginBottom: 16,
+        },
+        typesContainer: {
+            flexDirection: 'row',
+            justifyContent: 'center',
+            marginBottom: 8,
+        },
+        type: {
+            paddingVertical: 10,
+            paddingHorizontal: 16,
+            borderRadius: 16,
+            marginHorizontal: 8,
+            borderWidth: 2,
+            borderColor: '#ffffff',
+        },
     });
 
-  return (
-    <FlatList style={styles.container}
-        data={[pokemon]}
-        keyExtractor={(item) => item.id.toString()}
-        renderItem={({ item }) => (
-            <>
-                <View style={styles.card}>
-                    <View style={styles.header}>
-                        <View style={styles.nameContainer}>
-                            <Text style={styles.heading}>{pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1)}</Text>
-                            <Text style={styles.hp}>{pokemon.stats[0].base_stat} HP</Text>
+    return (
+        <FlatList style={styles.container}
+            data={[pokemon]}
+            keyExtractor={(item) => item.id.toString()}
+            renderItem={({ item }) => (
+                <>
+                    <View style={styles.card}>
+                        <View style={styles.header}>
+                            <View style={styles.nameContainer}>
+                                <Text style={styles.heading}>{pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1)}</Text>
+                                <Text style={styles.hp}>{pokemon.stats[0].base_stat} HP</Text>
+                            </View>
                         </View>
+
+                        <LinearGradient
+                            colors={gradientColors}
+                            start={{ x: 0, y: 0}}
+                            end={{ x: 1, y: 1}}
+                            style={styles.imageContainer}
+                        >
+                            <Image
+                                style={[styles.image, pokemonColors[0]]}
+                                source={{ uri: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.id}.png` }}
+                            />
+                        </LinearGradient>
+
+                        <View style={styles.typesContainer}>
+                            {pokemon.types.map((type) => (
+                                <View key={type.type.name} style={[styles.type, ...getTypeBackgroundStyle([type])]}>
+                                    <Text style={{ color: getTypeStyle(type.type.name).color, fontSize: 16, fontWeight: '600' }}>
+                                        {type.type.name}
+                                    </Text>
+                                </View>
+                            ))}
+                        </View>
+                        <PokemonStats stats={pokemon.stats} />
                     </View>
-                    <LinearGradient
-                        colors={gradientColors}
-                        start={{ x: 0, y: 0}}
-                        end={{ x: 1, y: 1}}
-                        style={styles.imageContainer}
-                    >
-                        <Image
-                            style={[styles.image, pokemonColors[0]]}
-                            source={{ uri: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.id}.png` }}
-                        />
-                    </LinearGradient>
-                    <View style={styles.typesContainer}>
-                        {pokemon.types.map((type) => (
-                            <View key={type.type.name} style={[styles.type, ...getTypeBackgroundStyle([type])]}>
-                                <Text style={{ color: getTypeStyle(type.type.name).color, fontSize: 16, fontWeight: '600' }}>
-                                    {type.type.name}
-                                </Text>
-                            </View>
-                        ))}
+
+                    <View>
+                      <Button title={isFavorite ? 'Remove from favorites' : 'Add to favorites'} onPress={handleFavoritePress} />
                     </View>
-                    <PokemonStats stats={pokemon.stats} />
-                </View>
-                <View>
-                  <Button title={isFavorite ? 'Remove from favorites' : 'Add to favorites'} onPress={handleFavoritePress} />
-                </View>
-                <View>
-                    {pokemonAbilities !== null && (
-                        pokemonAbilities.map((ability) => (
-                            <View key={ability.name}>
-                                <Text>{ability.name}</Text>
-                                <Text>{ability.definition}</Text>
-                            </View>
-                        ))
-                    )}
-                </View>
-            </>
-        )}
-    />
-  );
+
+                    <View>
+                        {pokemonAbilities !== null && (
+                            pokemonAbilities.map((ability) => (
+                                <View key={ability.name}>
+                                    <Text>{ability.name}</Text>
+                                    <Text>{ability.definition}</Text>
+                                </View>
+                            ))
+                        )}
+                    </View>
+
+                </>
+            )}
+        />
+    );
 };
 
 export default DetailsScreen;
