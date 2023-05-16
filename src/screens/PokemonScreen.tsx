de7c2cc9b15a5a23ad5aee335ac84986dc20ea9c
escheive <scheiveerik@gmail.com>
2023-05-15 17:41:53 -0700

    added a filter for pokemon to filter them by generation, still work in progress

--- src/screens/PokemonScreen.tsx
+++ src/screens/PokemonScreen.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { View, Text, StyleSheet, Button, FlatList, TouchableOpacity, Image, Dimensions } from 'react-native';
+import { View, Text, StyleSheet, Button, FlatList, TouchableOpacity, Image, Dimensions, Switch } from 'react-native';
 import { StackNavigationProp } from '@react-navigation/stack';
 import { Pokemon } from '../types';
 import { getTypeStyle } from '../utils/typeStyle';
@@ -10,12 +10,44 @@ type Props = {
     typeData: { [key: string]: typeData };
 };
 
+const versionOptions = [
+    { key: 'yellow', label: 'Red/Blue' },
+    { key: 'silver', label: 'Silver/Gold' },
+];
+
 const PokemonScreen = ({ navigation, pokemonList, typeData }: Props) => {
     const [selectedVersions, setSelectedVersions] = useState<string[]>([]);
 
+    // function to handle user press on a pokemon
     const handlePress = async (pokemon: Pokemon) => {
         navigation.navigate('Details', { pokemon });
     };
+
+    // function to handle selected generations for filter
+    const handleVersionSelect = (version: string) => {
+        if (selectedVersions.includes(version)) {
+            setSelectedVersions(selectedVersions.filter((v) => v !== version));
+        } else {
+            setSelectedVersions([...selectedVersions, version]);
+        }
+    };
+
+    // function to handle the filtering of pokemon
+    const filterPokemonByVersions = (pokemonList: Pokemon[], selectedVersions: string[]) => {
+        if (selectedVersions.length === 0) {
+            return pokemonList;
+        }
+
+        return pokemonList.filter((pokemon) =>
+            pokemon.game_indices.map((gameIndex) => gameIndex.version.name).some((version) =>
+                selectedVersions.includes(version)
+            )
+        );
+    };
+
+
+    const filteredPokemon = filterPokemonByVersions(pokemonList, selectedVersions);
+
 
     const renderItem = ({ item: pokemon }: { item: Pokemon }) => {
         const screenWidth = Dimensions.get('window').width;
@@ -37,53 +69,78 @@ const PokemonScreen = ({ navigation, pokemonList, typeData }: Props) => {
         );
     };
 
-  return (
-    <View style={styles.container}>
-      <FlatList
-        data={pokemonList}
-        renderItem={renderItem}
-        keyExtractor={(item) => item.name}
-        contentContainerStyle={styles.listContainer}
-        numColumns={2}
-      />
-    </View>
-  );
-}
+
+return (
+        <View style={styles.container}>
+            <View style={styles.filterContainer}>
+                <Text>Filter by Versions:</Text>
+                {versionOptions.map((option) => (
+                    <View key={option.key} style={styles.switchContainer}>
+                        <Switch
+                            value={selectedVersions.includes(option.key)}
+                            onValueChange={() => handleVersionSelect(option.key)}
+                        />
+                        <Text>{option.label}</Text>
+                  </View>
+                ))}
+
+
+
+            </View>
+            <FlatList
+                data={filteredPokemon}
+                renderItem={renderItem}
+                keyExtractor={(item) => item.name}
+                contentContainerStyle={styles.listContainer}
+                numColumns={2}
+            />
+        </View>
+    );
+};
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: 'white',
-  },
-  listContainer: {
-    padding: 5,
-  },
-  itemContainer: {
-    marginVertical: 10,
-    aspectRatio: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    borderWidth: 1,
-    borderColor: '#ccc',
-    borderRadius: 5,
-    margin: 5,
-  },
-  itemCard: {
-    height: '100%',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  itemName: {
-    fontSize: 18,
-    marginVertical: 5,
-    color: 'white',
-  },
-  image: {
-    width: 100,
-    height: 100,
-  },
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: 'white',
+    },
+    filterContainer: {
+        flexDirection: 'row',
+        alignItems: 'center',
+    },
+    switchContainer: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        marginHorizontal: 10,
+    },
+    listContainer: {
+        padding: 5,
+    },
+    itemContainer: {
+        marginVertical: 10,
+        aspectRatio: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        borderWidth: 1,
+        borderColor: '#ccc',
+        borderRadius: 5,
+        margin: 5,
+    },
+    itemCard: {
+        height: '100%',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    itemName: {
+        fontSize: 18,
+        marginVertical: 5,
+        color: 'white',
+    },
+    image: {
+        width: 100,
+        height: 100,
+    },
 });
 
 export default PokemonScreen;
