fa3c8a3da98186e2d8cb6abb04621bb2d0fe793d
escheive <scheiveerik@gmail.com>
2023-11-11 17:04:48 -0800

    created a new branch to recreate the app with expo, redux toolkit, and a couple of other updated techs

--- src/utils/database/database.tsx
+++
@@ -1,9 +0,0 @@
-// Database
-import SQLite from 'react-native-sqlite-storage';
-
-
-// Open the database
-export const database = SQLite.openDatabase({
-    name: 'Pokemon.db',
-    location: 'default',
-});
