d3c08faf4a60d5515015c80758abb20cd48ccd30
escheive <scheiveerik@gmail.com>
2023-11-20 13:25:15 -0800

    restructured fetchabilities data function, non operational right now

--- src/services/abilitiesService.tsx
+++ src/services/abilitiesService.tsx
@@ -41,62 +41,115 @@ const fetchAbilitiesFromAPI = async (start, end) => {
 
 // Function to fetch ability data from database or api
 const fetchAbilitiesData = () => {
-    console.log('fetchAbilitiesData function hit');
-    return async (dispatch) => {
-        dispatch(fetchAbilitiesRequest());
-        try {
-            // Wait for the table creation process to complete
-            createAbilitiesTable()
-              .then(async () => {
+  console.log('fetchAbilitiesData function hit');
+  return async (dispatch) => {
+    dispatch(fetchAbilitiesRequest());
 
-                const totalCount = 298;
-                const batchSize = 20;
-                const batches = Math.ceil(totalCount / batchSize);
-                const fetchedAbilitiesData = [];
+    try {
+      // Wait for the table creation process to complete
+      await createAbilitiesTable();
 
-                const start = 0;
-                const end = 298;
+      const totalCount = 298;
+      const start = 0;
+      const end = 298;
 
-                const hasData = await new Promise((resolve, reject) => {
-                    database.transaction((tx) => {
-                        tx.executeSql(
-                            `SELECT * FROM Abilities WHERE id BETWEEN ? AND ?;`,
-                            [start, end],
-                            (tx, result) => {
-                                if (result.rows.length > 0) {
-                                    for (let i=0; i<result.rows.length; i++) {
-                                        fetchedAbilitiesData.push(result.rows.item(i));
-                                    }
-                                }
-                                resolve(result.rows.length > 0);
-                            },
-                            (error) => {
-                                console.error('Error checking Ability data in the fetchAbilitiesData function, hasData subsection:', error);
-                                reject(error);
-                            }
-                        );
-                    });
-                });
-
-                if (!hasData) {
-                    // set loading phase so that loading screen updates
-                    const fetchedData = await fetchAbilitiesFromAPI(start, end);
-                    fetchedAbilitiesData.push(...fetchedData);
-                    await insertAbility(fetchedData);
+      const fetchedAbilitiesData = await new Promise((resolve, reject) => {
+        database.transaction((tx) => {
+          tx.executeSql(
+            `SELECT * FROM Abilities WHERE id BETWEEN ? AND ?;`,
+            [start, end],
+            (tx, result) => {
+              const abilitiesData = [];
+              if (result.rows.length > 0) {
+                for (let i=0; i<result.rows.length; i++) {
+                  fetchedAbilitiesData.push(result.rows.item(i));
                 }
+              }
+              resolve(result.rows.length > 0);
+            },
+            (error) => {
+              console.error('Error checking Ability data in the fetchAbilitiesData function, hasData subsection:', error);
+              reject(error);
+            }
+          );
+        });
+      });
 
-                console.log('Successfully fetched data in the fetchAbilitiesData function');
-                dispatch(fetchAbilitiesSuccess(fetchedAbilitiesData));
-            })
-              .catch((error) => {
-                console.error('Error fetching and inserting Abilities data in the fetchAbilitiesData function:', error);
-                dispatch(fetchAbilitiesFailure(error));
-            });
-        } catch (error) {
-            console.error('Error fetching and inserting Abilities data in the fetchAbilitiesData function:', error);
-            dispatch(fetchAbilitiesFailure(error));
-        }
-    };
+      if (!fetchedAbilitiesData.length) {
+        // set loading phase so that loading screen updates
+        const apiData = await fetchAbilitiesFromAPI(start, end);
+        // Insert fetched ability into database
+        await insertAbility(apiData);
+        // Push fetched abilities into our fetchedAbilitiesData array
+        fetchedAbilitiesData.push(...apiData);
+      }
+
+      console.log('Successfully fetched data in the fetchAbilitiesData function');
+      dispatch(fetchAbilitiesSuccess(fetchedAbilitiesData));
+    } catch(error) {
+      console.error('Error fetching and inserting Abilities data in the fetchAbilitiesData function:', error);
+      dispatch(fetchAbilitiesFailure(error));
+    }
+  };
 };
+// // Function to fetch ability data from database or api
+// const fetchAbilitiesData = () => {
+//     console.log('fetchAbilitiesData function hit');
+//     return async (dispatch) => {
+//         dispatch(fetchAbilitiesRequest());
+//         try {
+//             // Wait for the table creation process to complete
+//             createAbilitiesTable()
+//               .then(async () => {
+//
+//                 const totalCount = 298;
+//                 const batchSize = 20;
+//                 const batches = Math.ceil(totalCount / batchSize);
+//                 const fetchedAbilitiesData = [];
+//
+//                 const start = 0;
+//                 const end = 298;
+//
+//                 const hasData = await new Promise((resolve, reject) => {
+//                     database.transaction((tx) => {
+//                         tx.executeSql(
+//                             `SELECT * FROM Abilities WHERE id BETWEEN ? AND ?;`,
+//                             [start, end],
+//                             (tx, result) => {
+//                                 if (result.rows.length > 0) {
+//                                     for (let i=0; i<result.rows.length; i++) {
+//                                         fetchedAbilitiesData.push(result.rows.item(i));
+//                                     }
+//                                 }
+//                                 resolve(result.rows.length > 0);
+//                             },
+//                             (error) => {
+//                                 console.error('Error checking Ability data in the fetchAbilitiesData function, hasData subsection:', error);
+//                                 reject(error);
+//                             }
+//                         );
+//                     });
+//                 });
+//
+//                 if (!hasData) {
+//                     // set loading phase so that loading screen updates
+//                     const fetchedData = await fetchAbilitiesFromAPI(start, end);
+//                     fetchedAbilitiesData.push(...fetchedData);
+//                     await insertAbility(fetchedData);
+//                 }
+//
+//                 console.log('Successfully fetched data in the fetchAbilitiesData function');
+//                 dispatch(fetchAbilitiesSuccess(fetchedAbilitiesData));
+//             })
+//               .catch((error) => {
+//                 console.error('Error fetching and inserting Abilities data in the fetchAbilitiesData function:', error);
+//                 dispatch(fetchAbilitiesFailure(error));
+//             });
+//         } catch (error) {
+//             console.error('Error fetching and inserting Abilities data in the fetchAbilitiesData function:', error);
+//             dispatch(fetchAbilitiesFailure(error));
+//         }
+//     };
+// };
 
 export { fetchAbilitiesData, fetchAbilitiesFromAPI };
