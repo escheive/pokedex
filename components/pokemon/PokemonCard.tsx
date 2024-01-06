55ca75338b6a96b95d4896a7a126a17e88132921
escheive <scheiveerik@gmail.com>
2024-01-06 14:59:02 -0800

    organized pokemoncard component by changing dynamic styles to inline and moving stylesheet below whole component

--- components/pokemon/PokemonCard.tsx
+++ components/pokemon/PokemonCard.tsx
@@ -8,190 +8,8 @@ export const PokemonCard = ({ pokemon, pokemonDetails }) => {
   const [selectedAbility, setSelectedAbility] = useState(null);
 
     const backgroundColor = pokemonColors[pokemon?.pokemon_v2_pokemontypes[0].pokemon_v2_type.name].backgroundColor;
+    const color = pokemonColors[pokemon?.pokemon_v2_pokemontypes[0].pokemon_v2_type.name].color;
 
-            // Stylesheet for this screen
-            const styles = StyleSheet.create({
-              card: {
-                flex: 1,
-                alignItems: 'center',
-                justifyContent: 'flex-start',
-                marginBottom: 20,
-                overflow: 'hidden',
-              },
-              imageContainer: {
-                height: (Dimensions.get('window').height * 0.30),
-                width: '100%',
-                paddingTop: 32,
-                justifyContent: 'center',
-                alignItems: 'center',
-              },
-              imageBottomBackground: {
-                position: 'absolute',
-                bottom: 0,
-                height: '50%',
-                width: '100%',
-                backgroundColor: 'white',
-                borderTopRightRadius: 24,
-                borderTopLeftRadius: 24,
-              },
-              image: {
-                width: '80%',
-                height: '90%',
-                resizeMode: 'contain',
-              },
-              nextPokemonButton: {
-                position: 'absolute',
-                top: '45%',
-                right: 0,
-              },
-              prevPokemonButton: {
-                position: 'absolute',
-                top: '45%',
-                left: 0,
-              },
-              idContainer: {
-                position: 'absolute',
-                top: 10,
-                right: 10,
-                borderRadius: 50,
-                backgroundColor: 'white',
-                padding: 1,
-                width: 60,
-                height: 60,
-                justifyContent: 'center',
-                alignItems: 'center',
-              },
-              idText: {
-                fontSize: 22,
-                fontWeight: 'bold',
-              },
-              bottomOfCard: {
-                flexDirection: 'column',
-                alignItems: 'center',
-                paddingHorizontal: 20,
-                paddingBottom: 10,
-                width: '100%',
-                backgroundColor: 'white',
-              },
-              heading: {
-                fontWeight: 'bold',
-                alignSelf: 'center',
-              },
-              nameContainer: {
-                flexDirection: 'row',
-                alignItems: 'center',
-                justifyContent: 'center',
-                width: '100%',
-                paddingBottom: 8,
-              },
-              pokedexEntry: {
-                width: '100%',
-                textAlign: 'center',
-                fontSize: 16,
-                marginBottom: 12,
-                padding: 5,
-              },
-              typesContainer: {
-                flexDirection: 'row',
-                justifyContent: 'center',
-                marginLeft: 16,
-              },
-              type: {
-                paddingVertical: 3,
-                paddingHorizontal: 16,
-                borderRadius: 32,
-                marginRight: 8,
-                flexShrink: 1,
-              },
-              pokedexCardColumnsContainer: {
-                width: '100%',
-                flexDirection: 'row',
-                justifyContent: 'space-around',
-              },
-              pokedexCardColumn: {
-                width: '40%',
-                flexDirection: 'column',
-              },
-              pokedexCardEntryTitle: {
-                color: '#777',
-                textAlign: 'left',
-                paddingLeft: 6,
-              },
-              pokedexCardEntryInfo: {
-                width: '100%',
-                textAlign: 'center',
-                color: '#555',
-                borderWidth: 1,
-                borderColor: '#ccc',
-                borderRadius: 10,
-                paddingHorizontal: 18,
-                paddingVertical: 10,
-                marginBottom: 8,
-              },
-              abilitiesContainer: {
-                width: '90%',
-              },
-              abilitiesTitle: {
-                textAlign: 'center',
-                fontSize: 18,
-                marginBottom: 20,
-                paddingBottom: 5,
-                borderBottomWidth: 1,
-                borderColor: '#ccc',
-              },
-              abilityContainer: {
-                backgroundColor: pokemonColors[pokemon?.pokemon_v2_pokemontypes[0].pokemon_v2_type.name].backgroundColor,
-                borderRadius: 15,
-                alignItems: 'center',
-                marginBottom: 15,
-                padding: 5,
-              },
-              abilityItem: {
-                fontSize: 16,
-                color: pokemonColors[pokemon?.pokemon_v2_pokemontypes[0].pokemon_v2_type.name].color,
-                fontWeight: 'bold',
-              },
-              modalContainer: {
-                flex: 1,
-                justifyContent: 'center',
-                alignItems: 'center',
-                backgroundColor: 'rgba(0, 0, 0, 0.5)',
-              },
-              modalContent: {
-                alignItems: 'center',
-                backgroundColor: '#ccc',
-                width: '90%',
-                borderRadius: 16,
-              },
-                modalTitle: {
-                textAlign: 'center',
-                width: '100%',
-                fontSize: 24,
-                fontWeight: 'bold',
-                borderTopLeftRadius: 16,
-                borderTopRightRadius: 16,
-                backgroundColor: pokemonColors[pokemon?.pokemon_v2_pokemontypes[0].pokemon_v2_type.name].backgroundColor,
-                color: 'white',
-                paddingVertical: 16,
-                },
-                modalDefinitionContainer: {
-                backgroundColor: '#fff',
-                width: '100%',
-                alignItems: 'center',
-                paddingHorizontal: 10,
-                borderBottomLeftRadius: 16,
-                borderBottomRightRadius: 16,
-                },
-                modalDefinition: {
-                  fontSize: 18,
-                  color: 'gray',
-                  padding: 10,
-                  width: '100%',
-                  borderRadius: 12,
-                  textAlign: 'center',
-                  marginVertical: 30,
-                },
-            });
 
   return (
     <View style={styles.card}>
@@ -243,10 +61,10 @@ export const PokemonCard = ({ pokemon, pokemonDetails }) => {
           {pokemonDetails.pokemon_v2_pokemonabilities.map((ability) => (
             <TouchableOpacity
               key={ability.pokemon_v2_ability.name}
-              style={styles.abilityContainer}
+              style={[styles.abilityContainer, { backgroundColor } ]}
               onPress={() => setSelectedAbility(ability.pokemon_v2_ability)}
             >
-              <Text style={styles.abilityItem}>{capitalizeString(ability.pokemon_v2_ability.name)}</Text>
+              <Text style={[styles.abilityItem, { color }]}>{capitalizeString(ability.pokemon_v2_ability.name)}</Text>
             </TouchableOpacity>
           ))}
       </View>
@@ -262,7 +80,7 @@ export const PokemonCard = ({ pokemon, pokemonDetails }) => {
             activeOpacity={1}
             onPress={() => {}}
           >
-            <Text style={styles.modalTitle}>{selectedAbility?.name}</Text>
+            <Text style={[styles.modalTitle, { backgroundColor } ]}>{selectedAbility?.name}</Text>
             <View style={styles.modalDefinitionContainer}>
               <Text style={styles.modalDefinition}>{selectedAbility?.pokemon_v2_abilityeffecttexts[0]?.short_effect}</Text>
             </View>
@@ -300,3 +118,184 @@ export const PokemonCard = ({ pokemon, pokemonDetails }) => {
     </View>
   );
 };
+
+// Stylesheet for this screen
+const styles = StyleSheet.create({
+  card: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'flex-start',
+    marginBottom: 20,
+    overflow: 'hidden',
+  },
+  imageContainer: {
+    height: (Dimensions.get('window').height * 0.30),
+    width: '100%',
+    paddingTop: 32,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  imageBottomBackground: {
+    position: 'absolute',
+    bottom: 0,
+    height: '50%',
+    width: '100%',
+    backgroundColor: 'white',
+    borderTopRightRadius: 24,
+    borderTopLeftRadius: 24,
+  },
+  image: {
+    width: '80%',
+    height: '90%',
+    resizeMode: 'contain',
+  },
+  nextPokemonButton: {
+    position: 'absolute',
+    top: '45%',
+    right: 0,
+  },
+  prevPokemonButton: {
+    position: 'absolute',
+    top: '45%',
+    left: 0,
+  },
+  idContainer: {
+    position: 'absolute',
+    top: 10,
+    right: 10,
+    borderRadius: 50,
+    backgroundColor: 'white',
+    padding: 1,
+    width: 60,
+    height: 60,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  idText: {
+    fontSize: 22,
+    fontWeight: 'bold',
+  },
+  bottomOfCard: {
+    flexDirection: 'column',
+    alignItems: 'center',
+    paddingHorizontal: 20,
+    paddingBottom: 10,
+    width: '100%',
+    backgroundColor: 'white',
+  },
+  heading: {
+    fontWeight: 'bold',
+    alignSelf: 'center',
+  },
+  nameContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: '100%',
+    paddingBottom: 8,
+  },
+  pokedexEntry: {
+    width: '100%',
+    textAlign: 'center',
+    fontSize: 16,
+    marginBottom: 12,
+    padding: 5,
+  },
+  typesContainer: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    marginLeft: 16,
+  },
+  type: {
+    paddingVertical: 3,
+    paddingHorizontal: 16,
+    borderRadius: 32,
+    marginRight: 8,
+    flexShrink: 1,
+  },
+  pokedexCardColumnsContainer: {
+    width: '100%',
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+  },
+  pokedexCardColumn: {
+    width: '40%',
+    flexDirection: 'column',
+  },
+  pokedexCardEntryTitle: {
+    color: '#777',
+    textAlign: 'left',
+    paddingLeft: 6,
+  },
+  pokedexCardEntryInfo: {
+    width: '100%',
+    textAlign: 'center',
+    color: '#555',
+    borderWidth: 1,
+    borderColor: '#ccc',
+    borderRadius: 10,
+    paddingHorizontal: 18,
+    paddingVertical: 10,
+    marginBottom: 8,
+  },
+  abilitiesContainer: {
+    width: '90%',
+  },
+  abilitiesTitle: {
+    textAlign: 'center',
+    fontSize: 18,
+    marginBottom: 20,
+    paddingBottom: 5,
+    borderBottomWidth: 1,
+    borderColor: '#ccc',
+  },
+  abilityContainer: {
+    borderRadius: 15,
+    alignItems: 'center',
+    marginBottom: 15,
+    padding: 5,
+  },
+  abilityItem: {
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+  modalContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'rgba(0, 0, 0, 0.5)',
+  },
+  modalContent: {
+    alignItems: 'center',
+    backgroundColor: '#ccc',
+    width: '90%',
+    borderRadius: 16,
+  },
+  modalTitle: {
+    textAlign: 'center',
+    width: '100%',
+    fontSize: 24,
+    fontWeight: 'bold',
+    borderTopLeftRadius: 16,
+    borderTopRightRadius: 16,
+    color: 'white',
+    paddingVertical: 16,
+  },
+  modalDefinitionContainer: {
+    backgroundColor: '#fff',
+    width: '100%',
+    alignItems: 'center',
+    paddingHorizontal: 10,
+    borderBottomLeftRadius: 16,
+    borderBottomRightRadius: 16,
+  },
+  modalDefinition: {
+    fontSize: 18,
+    color: 'gray',
+    padding: 10,
+    width: '100%',
+    borderRadius: 12,
+    textAlign: 'center',
+    marginVertical: 30,
+  },
+});
